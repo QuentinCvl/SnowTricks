79bbf087920740f3ad6f4320b1beb898393029b0
Quentin Cuvelier <quentincuvelier@laposte.net>
2022-05-24 09:29:17 +0200

    Add link to each trick detail view, get this data in Controller

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -5,18 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Trick;
 
 class TricksController extends AbstractController
 {
   /**
    * @Route("/tricks/detail/{slug}", name="tricks_detail")
    */
-  public function index($slug = false): Response
+  public function index(string $slug): Response
   {
+    $repo = $this->getDoctrine()->getRepository(Trick::class);
+    $trick = $repo->findOneBy(['slug' => $slug]);
+
+    if (!$trick) {
+      throw $this->createNotFoundException(
+        'No tricks found for slug '.$slug
+      );
+    }
+
     return $this->render('tricks/detail.html.twig', [
       'controller_name' => 'TricksController',
-      'title' => "Tricks $slug",
-      'slug' => $slug
+      'title' => 'Figure n°'.$trick->getId(),
+      'trick' => $trick
     ]);
   }
 }
