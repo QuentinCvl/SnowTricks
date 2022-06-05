a9b4440b404f989e84efab04a19bf907d31a2877
Quentin Cuvelier <quentincuvelier@laposte.net>
2022-06-05 18:42:37 +0200

    Detail trick view main content

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Trick;
+use App\Repository\TrickRepository;
 
 class HomeController extends AbstractController
 {
   /**
    * @Route("/", name="home")
    */
-  public function index(): Response
+  public function index(TrickRepository $repo): Response
   {
-    $repo = $this->getDoctrine()->getRepository(Trick::class);
     $tricks = $repo->findAll();
 
     return $this->render('home/index.html.twig', [
--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TrickRepository;
 use App\Entity\Trick;
 
 class TricksController extends AbstractController
@@ -12,20 +13,11 @@ class TricksController extends AbstractController
   /**
    * @Route("/tricks/detail/{slug}", name="tricks_detail")
    */
-  public function index(string $slug): Response
+  public function index(Trick $trick): Response
   {
-    $repo = $this->getDoctrine()->getRepository(Trick::class);
-    $trick = $repo->findOneBy(['slug' => $slug]);
-
-    if (!$trick) {
-      throw $this->createNotFoundException(
-        'No tricks found for slug '.$slug
-      );
-    }
-
     return $this->render('tricks/detail.html.twig', [
       'controller_name' => 'TricksController',
-      'title' => 'Figure n°'.$trick->getId(),
+      'title' => 'Figure n°'. $trick->getId(),
       'trick' => $trick
     ]);
   }
